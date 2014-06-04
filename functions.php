c91f15dbaed7d3e55c105037012740e7166c1d47
Dave Santos <info@davesantos.com>
2014-06-03 20:04:30 -0700

    Cleaned whitespace, updated comments

--- functions.php
+++ functions.php
@@ -15,17 +15,17 @@ automatic_feed_links();
 
 // Clean up the <head>
 function removeHeadLinks() {
-	remove_action('wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+	remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
 	remove_action('wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
-	remove_action('wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-	remove_action('wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+	remove_action('wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
 	remove_action('wp_head', 'index_rel_link' ); // index link
 	remove_action('wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+	remove_action('wp_head', 'rel_canonical');
+	remove_action('wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
 	remove_action('wp_head', 'start_post_rel_link', 10, 0 ); // start link
-	remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+	remove_action('wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
 	remove_action('wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
 	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
-	remove_action('wp_head', 'rel_canonical');
 }
 add_action('init', 'removeHeadLinks');
 
@@ -34,9 +34,11 @@ function continue_reading_link() {
 	return ' <a href="'. get_permalink() . '">' . __( 'Continue reading <span class="meta-nav">&rarr;</span>' ) . '</a>';
 }
 
+# Change read more to ellipsis
 function auto_excerpt_more( $more ) {
 	return ' &hellip;' . continue_reading_link();
 }
+
 
 function custom_excerpt_more( $output ) {
 	if ( has_excerpt() && ! is_attachment() ) {
@@ -45,7 +47,7 @@ function custom_excerpt_more( $output ) {
 	return $output;
 }
 
-# Remove inline styles printed when the gallery shortcode is used.*
+# Remove inline styles printed when the gallery shortcode is used.
 function remove_gallery_css( $css ) {
 	return preg_replace( "#<style type='text/css'>(.*?)</style>#s", '', $css );
 }
@@ -55,6 +57,7 @@ function filter_ptags_on_images($content){
     return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 
+# Change locations of style.css
 # http://wordpress.org/support/topic/changing-location-of-stylecss-with-filters
 function wpi_stylesheet_dir_uri($stylesheet_dir_uri, $theme_name){
 	$subdir = '/css';
